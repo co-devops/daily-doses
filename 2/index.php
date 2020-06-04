3512b426bee4a4d40359f0e9e3101035361423c0
Jagdeep Singh <jdecode@gmail.com>
2020-06-04 22:07:02 +0530

    Add Sentry as error-handler

--- 2/index.php
+++ 2/index.php
@@ -1,11 +1,16 @@
 <?php
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ServerRequestInterface;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
 
+Sentry\init(['dsn' => getenv('SENTRY_DSN') ]);
+
 $app = AppFactory::create();
+
+$app->addRoutingMiddleware();
 
 $app->get('/', function (Request $request, Response $response, array $args) {
     $response->getBody()->write("Hello!");
@@ -17,5 +22,27 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
     $response->getBody()->write("Hello, $name");
     return $response;
 });
+
+// Setup Sentry
+$sentry = function (
+    ServerRequestInterface $request,
+    Throwable $exception
+) use ($app) {
+    Sentry\captureException($exception);
+    $payload = ['error' => $exception->getMessage()];
+    $response = $app->getResponseFactory()->createResponse();
+    $response->getBody()->write(
+        json_encode($payload, JSON_UNESCAPED_UNICODE)
+    );
+
+    return $response
+        ->withHeader('Content-Type', 'application/json')
+        ->withStatus(404);
+};
+
+// Sentry will handle errors
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorMiddleware->setDefaultErrorHandler($sentry);
+
 
 $app->run();
